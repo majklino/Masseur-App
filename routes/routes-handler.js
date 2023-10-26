7d48d99ea1c315f89ddfe8b145bd97d0e2263e24
unknown <mcervinka17@gmail.com>
2023-10-26 17:06:02 +0200

    modified config file structure

---
+++ routes/routes-handler.js
@@ -0,0 +1,10 @@
+function defineRoutes(app) {
+    app.get('/', (req, res) => {
+        res.send('hi :)');
+    });
+
+    // app.use('/login', require('./login/login-routes'));
+    // app.use('/register', require('./register/register-routes'));
+}
+
+module.exports = defineRoutes;
