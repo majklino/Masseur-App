7374e772dc340d95812cd317929c3c9675fc060d
Červinka <mcervinka17@gmail.com>
2023-10-27 12:32:36 +0200

    added new endpoints

--- routes/reservations/reservation-routes.js
+++ routes/reservations/reservation-routes.js
@@ -14,4 +14,29 @@ router.post('/check-availability', async function(req, res) {
     
 });
 
+router.post('/make-new-reservation', async function(req, res) {
+    res.json({response: 'yo!'});
+    
+});
+
+router.post('/make-new-reservation-unregistered', async function(req, res) {
+    res.json({response: 'yo!'});
+    
+});
+
+router.post('/cancel-reservation', async function(req, res) {
+    res.json({response: 'yo!'});
+    
+});
+
+router.post('/cancel-reservation-unregistered', async function(req, res) {
+    res.json({response: 'yo!'});
+    
+});
+
+router.post('/get-reservations-for-day', async function(req, res) {
+    res.json({response: 'yo!'});
+    
+});
+
 module.exports = router;
